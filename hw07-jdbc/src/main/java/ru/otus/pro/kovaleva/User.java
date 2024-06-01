a3b0a2a17c6f0f8bb75ea6a0cbc1d7186e8d8a4d
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2024-06-01 15:44:01 +0300

    added lombok

--- hw07-jdbc/src/main/java/ru/otus/pro/kovaleva/User.java
+++ hw07-jdbc/src/main/java/ru/otus/pro/kovaleva/User.java
@@ -1,5 +1,14 @@
 package ru.otus.pro.kovaleva;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString(includeFieldNames = true)
 @RepositoryTable(title = "users")
 public class User {
     @RepositoryIdField
@@ -15,41 +24,6 @@ public class User {
     @RepositoryField
     private String nickname;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public User() {
-    }
-
     public User(String login, String password, String nickname) {
         this.login = login;
         this.password = password;
@@ -61,15 +35,5 @@ public class User {
         this.login = login;
         this.password = password;
         this.nickname = nickname;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", password='" + password + '\'' +
-                ", nickname='" + nickname + '\'' +
-                '}';
     }
 }

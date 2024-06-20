519ba6d9ac643274874a831e237de70332295e9e
{ElenaKovaleva} <{lenshemeta@gmail.com}>
2024-06-20 10:20:52 +0300

    added homework

---
+++ hw08-http-server/src/main/java/ru/otus/pro/kovaleva/application/processors/HelloWorldRequestProcessor.java
@@ -0,0 +1,17 @@
+package ru.otus.pro.kovaleva.application.processors;
+
+import ru.otus.pro.kovaleva.HttpRequest;
+import ru.otus.pro.kovaleva.processors.RequestProcessor;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class HelloWorldRequestProcessor implements RequestProcessor {
+    @Override
+    public void execute(HttpRequest httpRequest, OutputStream output) throws IOException {
+        // CRLF
+        String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello World!!!</h1></body></html>";
+        output.write(response.getBytes(StandardCharsets.UTF_8));
+    }
+}
